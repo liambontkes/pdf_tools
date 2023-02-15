55bcc7b63de73671da1ba56ca35801fbf5261585
liamb <liambontkes1@gmail.com>
2023-02-15 15:26:49 -0800

    adjusted calibration search to include indicator type instruments

--- calibration.py
+++ calibration.py
@@ -10,15 +10,15 @@ def get_search_strings(tag):
     ls_search = []
 
     # only search for transmitter and indicator tags
-    if re.search(r"[TE]\d+\w?", tag):
-        logging.debug(f"Tag {tag} is type transmitter or indicator, calibration required")
+    if re.search(r"[TIE]\d+\w?", tag):
+        logging.debug(f"Tag {tag} is type transmitter/indicator/emitter, calibration required")
 
         # only search for last 3 sections of the tag
         ls_search.append(re.search(r"\w+.\w+[-_]\w+|\w+$", tag).group(0))
 
     # if tag is not correct type, return not applicable
     else:
-        logging.debug(f"Tag {tag} is not type transmitter or indicator, calibration NOT required")
+        logging.debug(f"Tag {tag} is not type transmitter/indicator/emitter, calibration NOT required")
         ls_search.append(constants.not_applicable)
 
     return ls_search
