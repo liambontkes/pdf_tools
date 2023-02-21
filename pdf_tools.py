0bd27c4613a1f6aa3844505d849bb47f9c1e7aaf
liamb <liambontkes1@gmail.com>
2023-02-21 11:52:44 -0800

    added documentation to pdf_tools.py and split_pdfs.py

--- pdf_tools.py
+++ pdf_tools.py
@@ -48,19 +48,33 @@ class PdfTool:
         self.supplier = self.input_folder.parent.name
 
     @staticmethod
-    def _timestamp():
+    def _timestamp() -> time.time:
+        """
+        Creates a timestamp at the current time.
+        :return: The current time.
+        """
         return time.time()
 
-    def start_timer(self):
+    def start_timer(self) -> None:
+        """
+        Sets the start time timestamp.
+        """
         self.start_time = self._timestamp()
 
-    def get_execution_time(self):
+    def get_execution_time(self) -> datetime.timedelta:
+        """
+        Gets the execution time of tool.
+        :return: The execution time since the timer started.
+        """
         execution_time = self._timestamp() - self.start_time
         return datetime.timedelta(seconds=execution_time)
 
-    @staticmethod
-    def get_pct_execution(n_processed, n_total):
-        return n_processed / n_total
-
-    def estimate_time_remaining(self, n_processed, n_total):
-        return self.get_execution_time() * (n_total - n_processed)
+    def log_execution(self, n_processed: int, n_total: int) -> None:
+        """
+        Logs the completion percentage and estimated time remaining.
+        :param n_processed: The number of processed items.
+        :param n_total: The total number of items to process.
+        """
+        pct_execution = n_processed / n_total * 100.0
+        estimated_time_remaining = self.get_execution_time() * (n_total - n_processed)
+        logging.info(f"{pct_execution}% of PDFs processed, estimated time remaining is {estimated_time_remaining}.")
--- search_pdfs.py
+++ search_pdfs.py
@@ -36,10 +36,6 @@ class SearchPdfs(pdf_tools.PdfTool):
             logging.info(f"Done searching in {pdf.name}!")
             logging.info(f"Time to process PDF: {self.get_execution_time()}")
 
-            logging.info(f"{self.get_pct_execution(n_processed=idx+1, n_total=len(self.ls_pdf))}% of PDFs processed, "
-                         f"estimated time "
-                         f"remaining is {self.estimate_time_remaining(n_processed=idx+1, n_total=len(self.ls_pdf))}.")
-
         logging.info(f"Done searching all PDFs.")
         return self.index
 
